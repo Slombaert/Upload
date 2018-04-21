511041f32165b53c047e1a734d61e5ac33d9a20b
Sylvain Lombaert <lomb.sylvain@gmail.com>
2018-04-21 18:21:08 +0200

    Create HTML structure

--- index.php
+++ index.php
@@ -22,11 +22,47 @@
             <input id='upload' name="upload[]" type="file" multiple="multiple" />
         </div>
 
-        <p><input type="submit" name="submit" value="Submit"></p>
+        <p><input type="submit" name="submit" value="Envoyer"></p>
 
     </form>
 
 </section>
+
+
+<section>
+
+
+
+    <div class="row">
+        <?php
+        $allImages = scandir('img/');
+        $images = array_diff($allImages, array('.','..'));
+        ?>
+
+        <?php
+        if (!empty($_GET['image'])) {
+            if (file_exists('img/'.$_GET['image'])) {
+                $deleteResult = unlink('img/'.$_GET['image']);
+                header('Location: index.php');
+            }
+        }
+        ?>
+
+        <?php foreach ($images as $image): ?>
+            <div class="col-sm-6 col-md-4">
+                <div class="thumbnail">
+                    <img src="<?= 'img/'.$image ?>" alt="<?= $image ?>">
+                    <div class="caption">
+                        <h3><?= $image ?></h3>
+                        <p><a href="?image=<?= $image ?>" class="btn btn-danger" role="button">Supprimer</a></p>
+                    </div>
+                </div>
+            </div>
+        <?php endforeach; ?>
+    </div>
+
+</section>
+
 
 </body>
 </html>
